f755c68300c45fa07a7d6db201b08da402e2f7c2
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-21 13:13:28 +0300

    Added the DataManager.

---
+++ ImageOfTheDayNasaApp/Managers/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  ImageOfTheDayNasaApp
+//
+//  Created by Simone Samardzhiev on 21.07.24.
+//
+
+import Foundation
+
+
+
+class DataManager: ObservableObject {
+    
+}
