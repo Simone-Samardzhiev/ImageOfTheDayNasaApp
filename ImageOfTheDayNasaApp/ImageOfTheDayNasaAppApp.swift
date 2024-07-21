0fbc566269ce5113e3c10ec97410849f4b9a3796
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-21 13:04:57 +0300

    Added the MainView.

--- ImageOfTheDayNasaApp/ImageOfTheDayNasaAppApp.swift
+++ ImageOfTheDayNasaApp/ImageOfTheDayNasaAppApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ImageOfTheDayNasaAppApp: App {
     var body: some Scene {
         WindowGroup {
+            MainView()
         }
     }
 }
---
+++ ImageOfTheDayNasaApp/Views/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  ImageOfTheDayNasaApp
+//
+//  Created by Simone Samardzhiev on 21.07.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
