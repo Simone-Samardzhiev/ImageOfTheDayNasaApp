aa327e6d98b737a139ca2ee0975da217181a288b
Simone Samardzhiev <simonesamardzhiev@gmail.com>
2024-07-21 12:47:52 +0300

    Initial Commit

---
+++ ImageOfTheDayNasaApp/ImageOfTheDayNasaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageOfTheDayNasaAppApp.swift
+//  ImageOfTheDayNasaApp
+//
+//  Created by Simone Samardzhiev on 21.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ImageOfTheDayNasaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
